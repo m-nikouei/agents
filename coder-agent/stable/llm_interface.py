26ba36329772b400b2a6dcf04573b7fb207f82ca
m-nikouei <mohammad.nikouei@gmail.com>
2025-06-26 21:29:19 -0400

    updated system message.

--- coder-agent/stable/llm_interface.py
+++ coder-agent/stable/llm_interface.py
@@ -2,7 +2,7 @@
 
 import os
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, AIMessage
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from config import read_configs
 
 configs = read_configs("/home/raha/agent_factory/coder-agent/config.json")
@@ -11,7 +11,10 @@ os.environ["OPENAI_API_KEY"] = configs["chatbot"]["OPENAI_API_KEY"]
 model = ChatOpenAI(model="gpt-4.1-2025-04-14", streaming=True)
 
 def predict(message, history):
-    history_langchain_format = []
+    history_langchain_format = [
+        SystemMessage(content="""You are Coder Agent, an expert AI assistant who writes clean and easy to understand code. At each step you need write the best code possible. different options are not necessary. Don't add comments to code.""")
+                      
+    ]
     for msg in history:
         if msg["role"] == "user":
             history_langchain_format.append(HumanMessage(content=msg["content"]))
@@ -22,4 +25,4 @@ def predict(message, history):
     response = ""
     for chunk in model.stream(history_langchain_format):
         response += chunk.content
-        yield response  # Gradio will stream tokens as they arrive!
+        yield response  # Gradio will stream token
